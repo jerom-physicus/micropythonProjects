47892353063615756c41c19b6bc0cdb4dbd02d45
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2024-12-17 00:04:18 +0530

    17/12/2024 00:04:16

--- sdcardtest2.py
+++ sdcardtest2.py
@@ -7,6 +7,13 @@ from sdcard import SDCard
 # MOSI -> GPIO 12
 # SCK  -> GPIO 14
 # CS   -> GPIO 27
+try:
+    os.umount('/sd')  # Unmount the SD card if it was previously mounted
+    print("Unmounted previous SD card.")
+except OSError:
+    pass  # If it's not mounted, we can ignore the error
+
+# Re-initialize the SD card
 sd = machine.SDCard(slot=2, sck=13, miso=12, mosi=14, cs=21)
 print("sdcard test 1...")
 os.mount(sd, "/sd")
